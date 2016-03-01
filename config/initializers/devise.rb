00158b50fc836b0c82ae014758ef9d5ca65fac1c
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-01 16:54:35 +0000

    omniauth_callbacks_controller.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,9 +1,9 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
-config.omniauth :facebook, "983788595037396", "APP_SECRET",
-callback_url: "CALLBACK_URL"
+
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-
+  config.omniauth :facebook, "983788595037396", "39b2d26aca857c0617818966b246f345",
+  callback_url: "CALLBACK_URL"
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
