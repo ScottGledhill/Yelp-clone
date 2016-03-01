16cec285760bb5cef11bdf8ac5a4e1ff61d1048c
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-01 14:42:26 +0000

    added uniqueness to restaurant creation

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -9,4 +9,9 @@ end
     restaurant = Restaurant.new(name: "kf")
     expect(restaurant).not_to be_valid
   end
+    it "is not valid unless it has a unique name" do
+    Restaurant.create(name: "Moe's Tavern")
+    restaurant = Restaurant.new(name: "Moe's Tavern")
+    expect(restaurant).to have(1).error_on(:name)
+  end
 end
